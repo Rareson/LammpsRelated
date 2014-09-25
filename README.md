50eea46f2459bbb2a8f09e652de332f9f893086c
Rareson <g1114128525@gmail.com>
2014-09-25 12:13:09 +0800

    installClass.sh finished with some test input files.

--- README.md
+++ README.md
@@ -5,9 +5,29 @@ LammpsRelated
 This repository is devoted to tricks closely related to LAMMPS.
 
 installClass.sh
--------------
+---------------
 The script "installClass" provides the minimum working environment for LAMMPS as
 required in my Computational Materials course.
 
 Usage:
+(Replace "~/Documents/code" by the path where you put the repository.)
+	cd ~/Documents/code
+	chmod +x ./installClass.sh
+	./installClass.sh
 
+After this, the lmp_serial is located in a file folder named "compiled".
+
+You can test it with:
+	(Replace "~/Documents/code" by the path where you put the repository.)
+
+	cd /Documents/code/testInput/
+	../compiled/lmp_serial<~/Documents/code/testInput/ex02.input
+
+
+installLAMMPS.sh
+---------------
+
+
+Acknowledgements
+----------------
+Cu_u3.eam and ex02.input are provided by my teacher.
---
+++ installClass.sh
@@ -0,0 +1,43 @@
+# This script is created by Domicor (g1114128525@gmail.com) to automate the
+# installation of LAMMPS and its dependencies.
+# Last updated Sep. 25, 2014.
+
+# Copyright (C) <2014>  <Domicor>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+workingDir=$(cd `dirname $0`;pwd) # the path where the script is located
+
+sudo apt-get install g++
+sudo apt-get install gfortran
+
+tar -xzvf lammps-28Jun14.tar.gz
+
+cd $workingDir/lammps-28Jun14/lib/meam
+make -f Makefile.gfortran
+
+cd $workingDir/lammps-28Jun14/lib/reax
+make -f Makefile.gfortran
+
+cd $workingDir/lammps-28Jun14/src
+make yes-meam
+make yes-reax
+
+cd $workingDir/lammps-28Jun14/src/STUBS
+make
+cd ..
+make serial
+
+mkdir $workingDir/compiled/
+cp lmp_serial $workingDir/compiled/lmp_serial
