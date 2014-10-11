fcf5d1fbf655168069eda0f31ce6e301b2677d16
Rareson <g1114128525@gmail.com>
2014-10-11 18:34:12 +0800

    Updated the gui.

---
+++ GUI.v.1.1.0.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file './QtGUI.ui'
+#
+# Created: Sat Oct 11 18:25:23 2014
+#      by: PyQt4 UI code generator 4.10.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+import subprocess
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(770, 604)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.horizontalLayout = QtGui.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.tabWidget = QtGui.QTabWidget(self.centralwidget)
+        self.tabWidget.setStyleSheet(_fromUtf8(""))
+        self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
+        self.Demo = QtGui.QWidget()
+        self.Demo.setObjectName(_fromUtf8("Demo"))
+        self.gridLayout = QtGui.QGridLayout(self.Demo)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.label = QtGui.QLabel(self.Demo)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.verticalLayout.addWidget(self.label)
+        self.textBrowser = QtGui.QTextBrowser(self.Demo)
+        self.textBrowser.setObjectName(_fromUtf8("textBrowser"))
+        self.verticalLayout.addWidget(self.textBrowser)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 2)
+        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.label_2 = QtGui.QLabel(self.Demo)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.verticalLayout_2.addWidget(self.label_2)
+        self.textBrowser_2 = QtGui.QTextBrowser(self.Demo)
+        self.textBrowser_2.setObjectName(_fromUtf8("textBrowser_2"))
+        self.verticalLayout_2.addWidget(self.textBrowser_2)
+        self.gridLayout.addLayout(self.verticalLayout_2, 1, 0, 1, 2)
+        self.comboBox = QtGui.QComboBox(self.Demo)
+        self.comboBox.setObjectName(_fromUtf8("comboBox"))
+        self.comboBox.addItem(_fromUtf8(""))
+        self.comboBox.addItem(_fromUtf8(""))
+        self.gridLayout.addWidget(self.comboBox, 2, 0, 1, 1)
+        self.pushButton = QtGui.QPushButton(self.Demo)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.gridLayout.addWidget(self.pushButton, 2, 1, 1, 1)
+        self.tabWidget.addTab(self.Demo, _fromUtf8(""))
+        self.tab = QtGui.QWidget()
+        self.tab.setObjectName(_fromUtf8("tab"))
+        self.tabWidget.addTab(self.tab, _fromUtf8(""))
+        self.tab_2 = QtGui.QWidget()
+        self.tab_2.setObjectName(_fromUtf8("tab_2"))
+        self.tabWidget.addTab(self.tab_2, _fromUtf8(""))
+        self.tab_4 = QtGui.QWidget()
+        self.tab_4.setObjectName(_fromUtf8("tab_4"))
+        self.gridLayout_2 = QtGui.QGridLayout(self.tab_4)
+        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.textBrowser_3 = QtGui.QTextBrowser(self.tab_4)
+        self.textBrowser_3.setObjectName(_fromUtf8("textBrowser_3"))
+        self.gridLayout_2.addWidget(self.textBrowser_3, 0, 0, 1, 1)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.label_3 = QtGui.QLabel(self.tab_4)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.horizontalLayout_2.addWidget(self.label_3)
+        self.pushButton_2 = QtGui.QPushButton(self.tab_4)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.horizontalLayout_2.addWidget(self.pushButton_2)
+        self.gridLayout_2.addLayout(self.horizontalLayout_2, 0, 1, 1, 1)
+        self.tabWidget.addTab(self.tab_4, _fromUtf8(""))
+        self.tab_5 = QtGui.QWidget()
+        self.tab_5.setObjectName(_fromUtf8("tab_5"))
+        self.tabWidget.addTab(self.tab_5, _fromUtf8(""))
+        self.tab_3 = QtGui.QWidget()
+        self.tab_3.setObjectName(_fromUtf8("tab_3"))
+        self.tabWidget.addTab(self.tab_3, _fromUtf8(""))
+        self.horizontalLayout.addWidget(self.tabWidget)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 770, 25))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(0)
+        QtCore.QObject.connect(self.comboBox, QtCore.SIGNAL(_fromUtf8("currentIndexChanged(int)")), self.textBrowser.reload)
+        QtCore.QObject.connect(self.comboBox, QtCore.SIGNAL(_fromUtf8("currentIndexChanged(int)")), self.textBrowser_2.reload)
+        QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("clicked()")), self.statusbar.show)
+        QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), self.installMin())
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "Lammps Unofficial GUI", None))
+        self.label.setText(_translate("MainWindow", "Script  Preview", None))
+        self.label_2.setText(_translate("MainWindow", "Notes", None))
+        self.comboBox.setItemText(0, _translate("MainWindow", "Lattice Constant", None))
+        self.comboBox.setItemText(1, _translate("MainWindow", "Elastic", None))
+        self.pushButton.setText(_translate("MainWindow", "Run", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.Demo), _translate("MainWindow", "Demo", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Homework", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Project", None))
+        self.label_3.setText(_translate("MainWindow", "Minimum", None))
+        self.pushButton_2.setText(_translate("MainWindow", "Compile", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), _translate("MainWindow", "Compile", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_5), _translate("MainWindow", "Preferences", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("MainWindow", "About", None))
+        
+    def installMin():
+        print('Install LAMMPS with package MEAM and REAX, please wait...')
+        subprocess.call(["chmod","+x","./installClass.sh"])
+        subprocess.call(["./installClass.sh"], shell=True)
+
+import sys
+from PyQt4.QtGui import *  
+from PyQt4.QtCore import *  
+if __name__ == "__main__":
+    
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(window)
+
+    window.show()
+    sys.exit(app.exec_())
